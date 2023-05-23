aa201e92404779e154badff43af548b1b3c394ea
Niko Raulas <niko.raulas@outlook.com>
2023-05-23 17:29:26 +0300

    modified db.py

--- db.py
+++ db.py
@@ -4,4 +4,5 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
