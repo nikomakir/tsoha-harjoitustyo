301f07458cb65a8686e5132ac8e7df79b22eb6e7
Niko Raulas <niko.raulas@outlook.com>
2023-05-26 18:48:03 +0300

    added function to delete place

--- places.py
+++ places.py
@@ -1,11 +1,17 @@
-from sqlalchemy.sql import text 
-from db import db   
+from sqlalchemy.sql import text
+from db import db
 
 
 def delete_review(review_id):
     sql = "DELETE FROM reviews WHERE id=:id"
     db.session.execute(text(sql), {"id":review_id})
     db.session.commit()
+
+def delete_place(place_id):
+    sql = "DELETE FROM exerciseplaces WHERE id=:id"
+    db.session.execute(text(sql), {"id":place_id})
+    db.session.commit()
+
 
 
 
