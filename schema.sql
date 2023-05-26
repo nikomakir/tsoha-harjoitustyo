aea10a03e7343de25c4cb8fa87f28b815ae196f5
Niko Raulas <niko.raulas@outlook.com>
2023-05-26 17:52:26 +0300

    added required tables to schema

--- schema.sql
+++ schema.sql
@@ -4,3 +4,39 @@ CREATE TABLE users (
     password TEXT,
     role INTEGER
 );
+
+CREATE TABLE exerciseplaces (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    address TEXT,
+    description TEXT
+    ON DELETE CASCADE
+);
+
+CREATE TABLE openinghours (
+    id SERIAL PRIMARY KEY,
+    exerciseplaces_id INTEGER REFERENCES exerciseplaces,
+    weekday TEXT,
+    opens INTEGER,
+    closes INTEGER
+);
+
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    exerciseplaces_id INTEGER REFERENCES exerciseplaces,
+    user_id INTEGER REFERENCES users,
+    stars INTEGER,
+    review TEXT,
+    time TIMESTAMP
+);
+
+CREATE TABLE groupnames (
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE
+);
+
+CREATE TABLE groups (
+    id SERIAL PRIMARY KEY,
+    exerciseplaces_id INTEGER REFERENCES exerciseplaces,
+    groupnames_id INTEGER REFERENCES groupnames
+);
