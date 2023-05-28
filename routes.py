02dc99cfefba1803a68ae084bf23ddce22acc956
Niko Raulas <niko.raulas@outlook.com>
2023-05-28 19:03:19 +0300

    pylint corrections

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
-from app import app
 from flask import render_template, request, redirect
+from app import app
 import users
 
 
@@ -10,15 +10,15 @@ def index():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
-        return render_template("login.html") 
-    
+        return render_template("login.html")
+
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
         if not users.login(username, password):
             return render_template("error.html", message="Väärä tunnus tai salasana")
         return redirect("/")
-    
+
 @app.route("/logout")
 def logout():
     users.logout()
@@ -28,24 +28,24 @@ def logout():
 def register():
     if request.method == "GET":
         return render_template("register.html")
-    
+
     if request.method == "POST":
         username = request.form["username"]
         if len(username) < 1 or len(username) > 20:
             return render_template("error.html", message="Tunnuksen tulee olla 1-20 merkkiä pitkä")
-        
+
         password1 = request.form["password1"]
         password2 = request.form["password2"]
         if password1 != password2:
             return render_template("error.html", message="Salasanat eroavat toisistaan")
         if password1 == "":
             return render_template("error.html", message="Salasana on tyhjä")
-        
+
         role = request.form["role"]
         if role not in ("1", "2"):
             return render_template("error.html", message="Tuntematon käyttäjärooli")
-        
+
         if not users.register(username, password1, role):
             return render_template("error.html", message="Rekisteröinti ei onnistunut")
-        
+
         return redirect("/")
--- users.py
+++ users.py
@@ -1,8 +1,8 @@
 import os
-from db import db
 from flask import request, session, abort
 from sqlalchemy.sql import text
 from werkzeug.security import check_password_hash, generate_password_hash
+from db import db
 
 
 def login(name, password):
